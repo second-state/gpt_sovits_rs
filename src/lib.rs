a632468f9f25de202ec97c955eb71ece44c4b4b8
csh <458761603@qq.com>
2024-12-25 14:08:42 +0800

    modify the `tch::no_grad` range

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@ impl GPTSovitsConfig {
     }
 
     pub fn build(&self, device: Device) -> anyhow::Result<GPTSovits> {
+        let _ = tch::no_grad_guard();
         let (cn_bert, g2pw) = match &self.cn_setting {
             Some((g2pw_path, cn_bert_path, tokenizer_path)) => {
                 let tokenizer = tokenizers::Tokenizer::from_file(&tokenizer_path)
@@ -142,24 +143,24 @@ impl GPTSovits {
         ref_audio_sr: usize,
         ref_text: &str,
     ) -> anyhow::Result<()> {
-        let mut gpt_sovits = tch::CModule::load_on_device(gpt_sovits_path, self.device)?;
-        gpt_sovits.set_eval();
-
-        // 避免句首吞字
-        let ref_text = if !ref_text.ends_with(['。', '.']) {
-            ref_text.to_string() + "."
-        } else {
-            ref_text.to_string()
-        };
-
-        let ref_audio = Tensor::from_slice(ref_audio_samples)
-            .to_device(self.device)
-            .unsqueeze(0);
-
-        let ref_audio_16k = self.resample(&ref_audio, ref_audio_sr, 16000)?;
-        let ref_audio_32k = self.resample(&ref_audio, ref_audio_sr, 32000)?;
-
         tch::no_grad(|| {
+            let mut gpt_sovits = tch::CModule::load_on_device(gpt_sovits_path, self.device)?;
+            gpt_sovits.set_eval();
+
+            // 避免句首吞字
+            let ref_text = if !ref_text.ends_with(['。', '.']) {
+                ref_text.to_string() + "."
+            } else {
+                ref_text.to_string()
+            };
+
+            let ref_audio = Tensor::from_slice(ref_audio_samples)
+                .to_device(self.device)
+                .unsqueeze(0);
+
+            let ref_audio_16k = self.resample(&ref_audio, ref_audio_sr, 16000)?;
+            let ref_audio_32k = self.resample(&ref_audio, ref_audio_sr, 32000)?;
+
             let ssl_content = self.ssl.forward_ts(&[&ref_audio_16k])?;
 
             let (ref_phone_seq, ref_bert_seq) = text::get_phone_and_bert(self, &ref_text)?;
@@ -180,18 +181,20 @@ impl GPTSovits {
     }
 
     pub fn resample(&self, audio: &Tensor, sr: usize, target_sr: usize) -> anyhow::Result<Tensor> {
-        let resample = self.ssl.method_is(
-            "resample",
-            &[
-                &IValue::Tensor(audio.shallow_clone()),
-                &IValue::Int(sr as i64),
-                &IValue::Int(target_sr as i64),
-            ],
-        )?;
-        match resample {
-            IValue::Tensor(resample) => Ok(resample),
-            _ => unreachable!(),
-        }
+        tch::no_grad(|| {
+            let resample = self.ssl.method_is(
+                "resample",
+                &[
+                    &IValue::Tensor(audio.shallow_clone()),
+                    &IValue::Int(sr as i64),
+                    &IValue::Int(target_sr as i64),
+                ],
+            )?;
+            match resample {
+                IValue::Tensor(resample) => Ok(resample),
+                _ => unreachable!(),
+            }
+        })
     }
 
     /// generate a audio tensor from text
