54de77506aa4c8fa25df3d94e243836f0549620b
csh <458761603@qq.com>
2025-01-18 03:56:18 +0800

    update default dict

--- src/text/dict.rs
+++ src/text/dict.rs
@@ -3,19 +3,23 @@ use std::collections::HashMap;
 use lazy_static::lazy_static;
 use std::path::PathBuf;
 
+static DEFAULT_ZH_WORD_DICT: &str = include_str!("../../resource/zh_word_dict.json");
+
 lazy_static! {
     static ref ZN_DICT: HashMap<String, Vec<String>> = {
-        if let Ok(word_dict_path) = std::env::var("GPT_SOVITS_DICT_PATH") {
-            let path = PathBuf::from(word_dict_path.as_str()).join("zh_word_dict.json");
+        let word_dict_path = std::env::var("GPT_SOVITS_DICT_PATH").unwrap_or(".".to_string());
+        let path = PathBuf::from(word_dict_path.as_str()).join("zh_word_dict.json");
+        if path.is_file() {
             let zh_word_dict = std::fs::read_to_string(path).unwrap();
             serde_json::from_str(&zh_word_dict).unwrap()
         } else {
-            HashMap::default()
+            serde_json::from_str(DEFAULT_ZH_WORD_DICT).unwrap()
         }
     };
     static ref EN_DICT: HashMap<String, Vec<String>> = {
-        if let Ok(word_dict_path) = std::env::var("GPT_SOVITS_DICT_PATH") {
-            let path = PathBuf::from(word_dict_path.as_str()).join("en_word_dict.json");
+        let word_dict_path = std::env::var("GPT_SOVITS_DICT_PATH").unwrap_or(".".to_string());
+        let path = PathBuf::from(word_dict_path.as_str()).join("en_word_dict.json");
+        if path.is_file() {
             let en_word_dict = std::fs::read_to_string(path).unwrap();
             serde_json::from_str(&en_word_dict).unwrap()
         } else {
